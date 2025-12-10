7d0183b0bd26b4f9430661e7d2772d4335e72966
DrMagenta <piotrhus.2005@gmail.com>
2025-12-10 20:24:32 +0100

    Added an endpoint for registering new users to the bookstore

--- LAB4/userhandler/server.py
+++ LAB4/userhandler/server.py
@@ -16,5 +16,29 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+@app.route('/api/register', methods=["POST"])
+def register_user():
+    email = request.get_json().get('email')
+    password = request.get_json().get('password')
+    
+    if not email:
+        return 'Email is required!', 400
+
+    elif not password:
+        return 'Password is required!', 400
+
+    else:
+        conn = get_db_connection()
+        conn.execute('INSERT INTO users (email, password) VALUES (?, ?)',
+                     (email, password))
+        
+        id = conn.execute('SELECT id FROM users WHERE email = ? AND password = ?',
+                     (email, password)).fetchone()
+        
+        id = id['id']
+        conn.commit()
+        conn.close()
+        return f'User was successfully added. User ID: {id}', 200
+
 if __name__ == '__main__':
     app.run(host="localhost", port=3003)
